30292b9ccb40c99f15555bb10809d42117ebe405
Alexita <alexahanthony@gmail.com>
2020-01-23 13:12:59 -0800

    google api and conversions

---
+++ public/js/conversions.js
@@ -0,0 +1,23 @@
+let flightMph = 550;
+let trainMph = 60;
+let busMph = 10;
+
+flightTime(function(data){
+    let time = googleApiDist/flightMph
+})
+flightTime();
+
+trainDistance(function(data){
+    let time = googleApiDist/trainMph
+})
+trainTime();
+
+busTime(function(data){
+    let time = googleApiDist/busMph
+})
+busTime();
+
+carTime(function(data){
+    let time = googleApiTime
+})
+carTime();
--- public/js/google-api.js
+++ public/js/google-api.js
@@ -1,12 +1,19 @@
 let googleApiKey = "AIzaSyB6YDdaRNCGktlvWISrGBjv75lcHfR-l44"
 let cityOne = "Seattle+WA"
 let cityTwo = "phoenix+az"
-let vehicleType = "flight"
+let vehicleType = "driving"
 
 let googleURL= "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + cityOne + "&destinations=" + cityTwo + "&mode=" + vehicleType + "&language=en-FR&key=" + googleApiKey;
 
 
 console.log(googleURL)
 
-driving
-transit
+// googleApiDist
+// googleApiTime
+
+$.ajax({
+    url: googleURL,
+    method:"GET"
+}).then(function(response){
+    console.log(JSON.stringify(response))
+})
