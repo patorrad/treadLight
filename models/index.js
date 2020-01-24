80cbb57f1dcc444eb62d4c2b988f01e221d28e58
Isabel Uriarte <uriarte.isabel@gmail.com>
2020-01-24 10:11:47 -0800

    changes made to index.js controllers and trip handlebars

--- models/index.js
+++ models/index.js
@@ -1,37 +1,8 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+$("#find-trip-info").submit(event=>{
+    event.preventDefault();
+    console.log("form submit!");
+    const depart = $('#departcity').val();
+    const arrive = $('#arrivecity').val();
+    //window.location.replace(`/retrieveTripData/${depart}/${arrive}`);
+    window.location.href = `/retrieveTripData/${depart}/${arrive}`;
+})
