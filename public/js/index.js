e721cc170055c0afc70d9f3af41b2724718bce40
Isabel Uriarte <uriarte.isabel@gmail.com>
2020-01-24 12:34:42 -0800

    fixed login form and made button functional

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,25 @@ $("#find-trip-info").submit(event=>{
     //window.location.replace(`/retrieveTripData/${depart}/${arrive}`);
     window.location.href = `/retrieveTripData/${depart}/${arrive}`;
 })
+console.log('indx.js');
+$("#loginForm").submit(event=>{
+    event.preventDefault();
+    const email = $("#email").val().trim();
+    const password = $("#password").val().trim();
+    const userLoginInfo={
+        email:email,
+        password:password
+    }
+    $.post('/login',userLoginInfo).then(data=>{
+       if(data.user){
+           window.location.href='/trip'
+       }
+       else {
+           alert('login failed!')
+       }
+    })
+    //make a post request to login
+    //then, if response says logged in, redirect to next page
+    //otherwise, just pop an alert that says falied
+
+})
